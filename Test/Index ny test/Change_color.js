653bbc98fd266fe3789c70ecd2f2a6bc41899087
Philip-456 <72019955+Philip-456@users.noreply.github.com>
2021-05-10 15:05:25 +0200

    swatchbook

---
+++ Test/Index ny test/Change_color.js
@@ -0,0 +1,16 @@
+function change_color(color){
+    var x = document.getElementById("change");
+    x.classList.remove("black");
+    x.classList.remove("cinnabar");
+    x.classList.remove("burnt-orange");
+    x.classList.remove("carrot-orange");
+    x.classList.remove("turbo");
+    x.classList.remove("lemon");
+    x.classList.remove("limerick");
+    x.classList.remove("water-course");
+    x.classList.remove("curious-blue");
+    x.classList.remove("governor-bay");
+    x.classList.remove("affair");
+    x.classList.remove("medium-violet-red");
+    x.classList.add(color);
+}
